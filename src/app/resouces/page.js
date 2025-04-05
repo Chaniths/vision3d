cec57fc67155698f046e41fa8c1d4e642e7aa937
Chaniths <wijekoonchanith12@gmail.com>
2025-04-05 20:58:27 +0530

    Login Page Add

---
+++ src/app/resouces/page.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Resources(){
+    return(
+        <>
+        <div className="">
+            <h2 className="text-[40px] font-medium p-4 mt-1 ">
+                Resouce & Support
+            </h2>
+
+        </div>
+        
+        </>
+    )
+};
